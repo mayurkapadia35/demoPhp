5c04219147612a92f0b0183b27d6553364043101
Mayur <lanetteam.mayur35@gmail.com>
2018-03-26 16:33:25 +0530

    Commit for strlen()...in welcome.php

--- welcome.php
+++ welcome.php
@@ -1,3 +1,15 @@
 <?php
 
     echo "Welcome to the php world";
+//    echo "<br>";
+//    $price="1236";
+//    if(strlen($price)<=4)
+//    {
+//        echo "Count is ".strlen($price)."\n";
+//        echo number_format($price, 0, '', '');
+//    }
+//    else if(strlen($price)>=5)
+//    {
+//        echo "Count is ".strlen($price)."\n";
+//        echo number_format($price, 0, '', ',');
+//    }
